54a474c03a2bd848ee96f77072f3e7f055fa8195
Your Name <your_email@example.com>
2024-11-04 20:10:19 +0530

    ui

--- frontend/src/Components/SideDrawer.jsx
+++ frontend/src/Components/SideDrawer.jsx
@@ -9,13 +9,14 @@ import { FaChevronDown, FaSearch } from 'react-icons/fa';
 import { FaBell } from 'react-icons/fa';
 import { HStack } from "@chakra-ui/react"
 import { Avatar } from "@/components/ui/avatar"
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
 import ProfileModel from './ProfileModel';
 import Drawer from './Drawer';
 import { removeChat } from '../utils/chatSlice';
 function SideDrawer({fetchchat}) {
   const navigate=useNavigate()
 const dispatch=useDispatch()
+const user=useSelector((store)=>store.user)
   async function handleLogOut(){
     try {
       const response=await axiosInstance({url:"/logout",method:"POST"})
@@ -43,7 +44,7 @@ dispatch(removeChat())
   <FaBell className='mr-2 text-2xl'/>
   
   <details className="dropdown ">
-  <summary className="btn m-1 bg-transparent ">  <Avatar  variant="solid" name="AKSHAY KUMAR" /></summary>
+  <summary className="btn m-1 bg-transparent ">  <Avatar  variant="solid" name={user.name} /></summary>
   <ul className="menu   dropdown-content gap-3 bg-base-100 rounded-box z-[1] w-20   shadow">
     <li className=" hover:bg-gray-200 p-2"><ProfileModel/></li>
     <li className=" hover:bg-gray-200 p-2" onClick={handleLogOut}>Logout</li>
